da0d338c198a955f57a9020c899c243187523000
wuyangfan <1102042793@qq.com>
2023-05-04 12:19:47 +0800

    解析模板生成ast语法树

---
+++ src/compiler/index.js
@@ -0,0 +1,117 @@
+const ncname = `[a-zA-Z_][\\-\\.0-9_a-zA-Z]*`;  // 标签名 abc  abc-tag
+const qnameCapture = `((?:${ncname}\\:)?${ncname})`  // <tag:sub></tag:sub>  或者 <abc></abc>
+// <div>
+const startTagOpen = new RegExp(`^<${qnameCapture}`)
+// </div>
+const endTag = new RegExp(`^<\\/${qnameCapture}[^>]*>`)
+
+// a=b  a="b" a='b'
+var attribute = /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<div>`]+)))?/;
+//  />  标签的结束
+const startTagClose = /^\s*(\/?)>/;
+
+const defaultTagRE = /\{\{((?:.|\r?\n)+?)\}\}/  //{{ msg }}
+
+function createAstElement(tagName, attrs) {
+    return {
+        tag: tagName,
+        children: [],
+        parent: null,
+        attrs
+    }
+}
+let root = null
+const stack = []
+function start(tagName, attrs) {
+    let parent = stack[stack.length - 1]
+    let element = createAstElement(tagName, attrs)
+    if (!root) {
+        root = element
+    }
+    element.parent = parent
+    if (parent) {
+        parent.children.push(element)
+    }
+    stack.push(element)
+}
+function end(tagName) {
+    let last = stack.pop()
+    if (last.tag !== tagName) {
+        throw new Error('标签有误')
+    }
+    last.tag = tagName
+}
+function chars(text) {
+    text = text.replace(/\s/g, '')
+    let parent = stack[stack.length - 1]
+    if (text) {
+
+        parent.children.push({
+            type: 3,
+            text
+        })
+    }
+}
+
+function parseHTML(html) {
+    function advance(len) {
+        html = html.slice(len)
+    }
+    function parseStartTag() {
+        const start = html.match(startTagOpen)
+        if (start) {
+            const match = {
+                tagName: start[1],
+                attrs: []
+            }
+            advance(start[0].length)
+            let end;
+            let attr;
+            // 到结尾了，说明没有属性需要处理
+            while (!(end = html.match(startTagClose)) && (attr = html.match(attribute))) {
+                console.log(attr);
+                match.attrs.push({
+                    name: attr[1],
+                    value: attr[3] || attr[4] || attr[5]
+                })
+                advance(attr[0].length)
+            }
+            if (end) {
+                advance(end[0].length)
+            }
+            return match
+        }
+        return false
+    }
+    while (html) {
+        let textEnd = html.indexOf('<')
+        if (textEnd == 0) {
+            const startTagMatch = parseStartTag(html)
+            if (startTagMatch) {
+                const { tagName, attrs } = startTagMatch
+                start(tagName, attrs)
+                continue
+            }
+            const endTagMatch = html.match(endTag)
+            if (endTagMatch) {
+                end(endTagMatch[1])
+                advance(endTagMatch[0].length)
+                continue
+            }
+        }
+        let text;
+        if (textEnd > 0) {
+            text = html.substring(0, textEnd)
+        }
+        if (text) {
+            chars(text)
+            advance(text.length)
+        }
+
+    }
+}
+
+export function compileToFunction(template) {
+    parseHTML(template)
+    console.log(root);
+}
--- src/init.js
+++ src/init.js
@@ -1,3 +1,4 @@
+import { compileToFunction } from './compiler/index'
 import { initState } from './state'
 
 export function initMixin(Vue) {
@@ -6,5 +7,24 @@ export function initMixin(Vue) {
         const vm = this
         vm.$options = options
         initState(vm)
+
+        if (vm.$options.el) {
+            vm.$mount(vm.$options.el)
+        }
     }
+    Vue.prototype.$mount = function (el) {
+        const vm = this
+        const options = vm.$options
+        el = document.querySelector(el)
+        if (!options.render) {
+            let template = options.template
+            if (!template && el) {
+                let template = el.outerHTML
+                console.log(template);
+                let render = compileToFunction(template)
+                options.render = render
+            }
+        }
+    }
+
 }
